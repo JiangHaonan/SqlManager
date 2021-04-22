7bc951cd34c884f8e8a12549870208c9c99300fb
mutou <357953710@qq.com>
2021-04-22 15:06:25 +0800

    Create V1.2__description.sql

---
+++ V1.2__description.sql
@@ -0,0 +1,1 @@
+INSERT INTO `user_center`.`user`(`id`, `wx_id`, `wx_nickname`, `roles`, `avatar_url`, `create_time`, `update_time`, `bonus`) VALUES (5, '', '', '', 'xxx', '2021-04-19 01:09:53', '2021-04-19 01:09:53', 100);
