2ee550823cc4ef152fff8bffc3c5f6869da18ee7
jianghn <357953710@qq.com>
2021-04-24 23:31:47 +0800

    delete nouse file

--- V0.1__description.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO `user_center`.`user`(`id`, `wx_id`, `wx_nickname`, `roles`, `avatar_url`, `create_time`, `update_time`, `bonus`) VALUES (3, '', '', '', 'xxx', '2021-04-19 01:09:53', '2021-04-19 01:09:53', 100);
